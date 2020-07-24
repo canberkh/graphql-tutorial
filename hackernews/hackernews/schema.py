d4cb9955d32eb0b19d658df2547037b1ccec9527
Canberk Hallik <canberkh@gmail.com>
2020-07-24 15:41:28 +0200

    format file

--- hackernews/hackernews/schema.py
+++ hackernews/hackernews/schema.py
@@ -7,7 +7,10 @@ class Query(links.schema.Query, graphene.ObjectType):
     pass
 
 
-class Mutation(links.schema.Mutation, graphene.ObjectType):
+class Mutation(
+    links.schema.Mutation, graphene.ObjectType,
+):
     pass
+
 
 schema = graphene.Schema(query=Query, mutation=Mutation)
