c853c799dc809af1b9b933e2e05c1005400a12d9
Canberk Hallik <canberkh@gmail.com>
2020-07-24 15:41:43 +0200

    Add user model and schema

--- hackernews/links/models.py
+++ hackernews/links/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 
+
 class Link(models.Model):
     url = models.URLField()
     description = models.TextField(blank=True)
 
+
+class User(models.Model):
+    username = models.CharField(max_length=40)
+    password = models.CharField(max_length=40)
+    email = models.EmailField()
--- hackernews/links/schema.py
+++ hackernews/links/schema.py
@@ -1,7 +1,8 @@
 import graphene
 from graphene_django import DjangoObjectType
+from django.contrib.auth import get_user_model
 
-from .models import Link
+from .models import Link, User
 
 
 class LinkType(DjangoObjectType):
@@ -9,35 +10,56 @@ class LinkType(DjangoObjectType):
         model = Link
 
 
-class Query(graphene.ObjectType):
-    links = graphene.List(LinkType)
-
-    def resolve_links(self, info, **kwargs):
-        return Link.objects.all()
-
-#1
 class CreateLink(graphene.Mutation):
     id = graphene.Int()
     url = graphene.String()
     description = graphene.String()
 
-    #2
+    # 2
     class Arguments:
         url = graphene.String()
         description = graphene.String()
 
-    #3
+    # 3
     def mutate(self, info, url, description):
         link = Link(url=url, description=description)
         link.save()
 
-        return CreateLink(
-            id=link.id,
-            url=link.url,
-            description=link.description,
-        )
+        return CreateLink(id=link.id, url=link.url, description=link.description,)
 
 
-#4
+class UserType(DjangoObjectType):
+    class Meta:
+        model = get_user_model()
+
+
+class CreateUser(graphene.Mutation):
+    user = graphene.Field(UserType)
+
+    class Arguments:
+        username = graphene.String(required=True)
+        password = graphene.String(required=True)
+        email = graphene.String(required=True)
+
+    def mutate(self, info, username, password, email):
+        user = get_user_model()(username=username, email=email,)
+        user.set_password(password)
+        user.save()
+
+        return CreateUser(user=user)
+
+
 class Mutation(graphene.ObjectType):
     create_link = CreateLink.Field()
+    create_user = CreateUser.Field()
+
+
+class Query(graphene.ObjectType):
+    links = graphene.List(LinkType)
+    users = graphene.List(UserType)
+
+    def resolve_links(self, info, **kwargs):
+        return Link.objects.all()
+
+    def resolve_users(self, info, **kwargs):
+        return get_user_model().objects.all()
